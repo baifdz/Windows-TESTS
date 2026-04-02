4c6d8f66636a2aa3a6728df46767fdeebd95dbfd
baifdz <alumnos3bird@gmail.com>
2026-04-02 13:40:31 -0600

    Delete ADusers.ps1

--- ADusers.ps1
+++
@@ -1,3 +0,0 @@
-$searcher = [adsisearcher]"(displayName=*)" 
-$searcher.Filter = "(&(objectCategory=person)(objectClass=user))"
-$searcher.FindAll() | Select-Object @{N='User';E={$_.Properties.samaccountname}}
